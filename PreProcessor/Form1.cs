71a7e97e3f1512e65f27eee31dfdedf5e0203d88
Николай <barness1985@gmail.com>
2021-10-02 12:00:44 +0300

    nice

--- PreProcessor/Form1.cs
+++ PreProcessor/Form1.cs
@@ -18,6 +18,11 @@ namespace PreProcessor
         public Form1()
         {
             InitializeComponent();
+            dataGridView2.Rows.Add();
+            dataGridView2.Rows[0].Cells[0].Value = 1;
+            dataGridView2.Rows[0].Cells[1].Value = 0;
+            construction.nodesLoad.Add(0);
+
         }
 
         private void tableLayoutPanel2_Paint(object sender, PaintEventArgs e)
@@ -36,18 +41,30 @@ namespace PreProcessor
             {
                 construction.kernels.RemoveAt(construction.kernels.Count-1);
                 dataGridView1.Rows.RemoveAt(dataGridView1.Rows.Count - 1);
-                //dataGridView2.Rows.RemoveAt(dataGridView1.Rows.Count - 1);
+                dataGridView2.Rows.RemoveAt(dataGridView2.Rows.Count - 1);
                 dataGridView3.Rows.RemoveAt(dataGridView3.Rows.Count - 1);
+                construction.nodesLoad.RemoveAt(construction.nodesLoad.Count - 1);
             }
             else
             {
                 construction.kernels.Add(new Kernel());
                 dataGridView1.Rows.Add();
+                dataGridView2.Rows.Add();
+                dataGridView2.Rows[dataGridView2.Rows.Count - 1].Cells[0].Value = dataGridView2.Rows.Count;
+                dataGridView2.Rows[dataGridView2.Rows.Count - 1].Cells[1].Value = 0;
+                construction.nodesLoad.Add(0);
+               
                 dataGridView3.Rows.Add();
                 for (int i=0;i<4;i++)
                 {
                     dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[i].Value = 1;
                    
+                }
+
+                for (int i = 0; i < 4; i++)
+                {
+                    dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[1].Value = 600;
+
                 }
 
 
@@ -58,15 +75,7 @@ namespace PreProcessor
 
         private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
         {
-            
-
-
-            
-
-
-
-
-
+      
         }
 
 
@@ -95,6 +104,12 @@ namespace PreProcessor
                     construction.kernels[i].E = double.Parse(dataGridView1.Rows[i].Cells[2].Value.ToString());
                     construction.kernels[i].b = double.Parse(dataGridView1.Rows[i].Cells[3].Value.ToString());
                     construction.kernels[i].distributedLoad = double.Parse(dataGridView3.Rows[i].Cells[1].Value.ToString());
+                }
+
+                construction.nodesLoad.Clear();
+                for (int i = 0; i < dataGridView2.Rows.Count; i++)
+                { 
+                    construction.nodesLoad.Add(double.Parse(dataGridView2.Rows[i].Cells[1].Value.ToString()));
                 }
             }
 
@@ -127,9 +142,9 @@ namespace PreProcessor
                 }
             }
 
-            if (max > 300)
+            if (max > 500)
             {
-                k = 300.0f / max;
+                k = 500.0f / max;
 
                 for (int i = 0; i < construction.kernels.Count; i++)
                 {
@@ -153,11 +168,11 @@ namespace PreProcessor
                     int h = construction.kernels[i].location.Height;
 
                     if (i == 0)
-                        construction.kernels[i].location = new Rectangle(50, axle - h / 2, (pictureBox1.Width - 80) / construction.totalWidth * construction.kernels[i].L, h);
+                        construction.kernels[i].location = new Rectangle(50, axle - h / 2, (pictureBox1.Width - 120) / construction.totalWidth * construction.kernels[i].L, h);
                     else
                     {
                         construction.kernels[i].location = new Rectangle(construction.kernels[i - 1].location.Width + construction.kernels[i - 1].location.X, axle
-                            - h / 2, (pictureBox1.Width - 80) / construction.totalWidth * construction.kernels[i].L, h);
+                            - h / 2, (pictureBox1.Width - 120) / construction.totalWidth * construction.kernels[i].L, h);
                     }
                 }
             }
@@ -256,7 +271,102 @@ namespace PreProcessor
 
 
 
+            if (construction.nodesLoad[0]>0)
+            {
+                int w = construction.kernels[0].location.X;
+                Pen Fpen = new Pen(Color.Black);
+                Fpen.CustomEndCap = new AdjustableArrowCap(6, 3);
+                Fpen.Width = 7;
+                g.DrawLine(Fpen, w, axle, w + 40, axle);
+            }
 
+            if (construction.nodesLoad[0]<0)
+            {
+                int w = construction.kernels[0].location.X;
+                Pen Fpen = new Pen(Color.Black);
+                Fpen.CustomEndCap = new AdjustableArrowCap(6, 3);
+                Fpen.Width = 7;
+                g.DrawLine(Fpen, w, axle, w - 40, axle);
+
+            }
+            for (int i=1;i<construction.nodesLoad.Count;i++)
+            {
+                if (construction.nodesLoad[i]>0)
+                {
+                    int w=construction.kernels[i - 1].location.X + construction.kernels[i - 1].location.Width;
+                    Pen Fpen = new Pen(Color.Black);
+                    Fpen.CustomEndCap = new AdjustableArrowCap(6, 3);
+                    Fpen.Width = 7;
+                    g.DrawLine(Fpen, w, axle, w + 40, axle);
+                }
+
+                if (construction.nodesLoad[i] < 0)
+                {
+                    int w = construction.kernels[i - 1].location.X + construction.kernels[i - 1].location.Width;
+                    Pen Fpen = new Pen(Color.Black);
+                    Fpen.CustomEndCap = new AdjustableArrowCap(6, 3);
+                    Fpen.Width = 7;
+                    g.DrawLine(Fpen, w, axle, w - 40, axle);
+                }
+
+            }
+
+            if (checkBox2.Checked)
+            {
+                // левый
+                if (construction.nodesLoad[0] >= 0)
+                {
+                    Pen Zp = new Pen(Color.Black);
+                    Zp.Width = 2;
+                    g.DrawLine(Zp, 48, axle - 30, 48, axle + 30);
+                    for (int i = 0; i < 6; i++)
+                    {
+                        int x = 43;
+                        int y = axle - 30 + 10;
+                        g.DrawLine(Zp, x - 2, y + (i * 10) + 2, x + 5, y + i * 10 - 10);
+                    }
+                }
+                else
+                    checkBox2.Checked = false;
+            }
+
+            if (checkBox1.Checked)
+            {
+                // правый 
+                if (construction.nodesLoad[construction.nodesLoad.Count - 1] <= 0)
+                {
+                    Pen Zp = new Pen(Color.Black);
+                    Zp.Width = 2;
+                    int leftborder = pictureBox1.Width - 70;
+                    g.DrawLine(Zp, leftborder + 2, axle - 30, leftborder + 2, axle + 30);
+                    for (int i = 0; i < 6; i++)
+                    {
+                        int x = leftborder;
+                        int y = axle - 30 - 10;
+                        g.DrawLine(Zp, x + 10, y + (i * 10) + 10, x, y + i * 10 + 20);
+                    }
+                }
+                else
+                    checkBox1.Checked = false;
+            }
+
+
+            // отрисовка текста нагрузок
+            for (int i = 0; i < construction.kernels.Count; i++)
+            {
+                if (construction.kernels[i].distributedLoad != 0)
+                {
+                    int x = construction.kernels[i].location.X + (construction.kernels[i].location.Width / 2);
+               
+                    Point pt = new Point(x, axle - 40);
+                   
+                    Font ft = new Font(label1.Font.Name,15);
+                    
+                    Brush bh = Brushes.Black;
+                    string st = "q= "+ construction.kernels[i].distributedLoad.ToString();
+                    g.DrawString(st, ft,bh, pt);
+                }
+            }
 
 
 
